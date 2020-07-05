7a05726f22b1264081c030719595e2cb26bcd18f
Vladimir <46341690+Qustomxyz@users.noreply.github.com>
2020-07-05 22:40:39 +0300

    task

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php
-        echo "Hello, hmm";
+        class Task {
+
+                protected $description;
+
+                protected $complited;
+
+                public __construct($description) {
+                        $this->description = $description
+                }
+        }
+
+$task = new Task('Go to go');
+
+var_dump($task);
