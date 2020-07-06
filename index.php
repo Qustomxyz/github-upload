ae3f80e9e5615813b752967369b97640140f206a
Vladimir <46341690+Qustomxyz@users.noreply.github.com>
2020-07-06 09:06:23 +0300

    fix ;

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 try {
         $pdo = new PDO('mysql:host:=127.0.0.1;dbname=larac', 'www', '117630');
 } catch(PDOexception $e) {
-        die('Could not connect.')
+        die('Could not connect.');
 }
 
 
