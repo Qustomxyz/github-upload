a49a3ad3457841ba0cdde6cfb73c752ad80946a8
Vladimir <46341690+Qustomxyz@users.noreply.github.com>
2020-07-06 16:04:10 +0300

    add options

--- database/Connection.php
+++ database/Connection.php
@@ -3,7 +3,7 @@
 class Connection {
     public static function make($conf) {
         try {
-            $pdo = new PDO("mysql:host=127.0.0.1;dbname=".$conf['dbname'], $conf['user'], $conf['psw']);
+            $pdo = new PDO("mysql:host=127.0.0.1;dbname=".$conf['dbname'], $conf['user'], $conf['psw'], $conf['options']);
         } catch(PDOexception $e) {
             die('Could not connect:'.$e->getMessage());
         }
