be6ed9f833ec60b649543fb5c50f7857468b0cae
Vladimir <46341690+Qustomxyz@users.noreply.github.com>
2020-07-06 14:40:25 +0300

    fix bug2

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $statment = $pdo->prepare('select * from todos');
 
 $statment->execute();
 
-$tasks = $statment->fetchAll(PDO::FETCH_OBJ)[0];
+$tasks = $statment->fetchAll(PDO::FETCH_OBJ);
 var_dump($tasks);
 
 require 'index.view.php';
