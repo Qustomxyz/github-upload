c3b3bf57146a8f3ffe5b559e386270b677b87d96
Vladimir <46341690+Qustomxyz@users.noreply.github.com>
2020-07-06 15:52:10 +0300

    refactor config transmition

--- bootstrap.php
+++ bootstrap.php
@@ -4,4 +4,4 @@ require 'database/Connection.php';
 require 'database/QueryBuilder.php';
 $conf = require 'config/config.php';
 
-return new QueryBuilder(Connection::make($conf));
+return new QueryBuilder(Connection::make($conf['db']));
--- database/Connection.php
+++ database/Connection.php
@@ -3,7 +3,7 @@
 class Connection {
     public static function make($conf) {
         try {
-            $pdo = new PDO("mysql:host=127.0.0.1;dbname=".$conf['db']['dbname'], $conf['db']['user'], $conf['db']['psw']);
+            $pdo = new PDO("mysql:host=127.0.0.1;dbname=".$conf['dbname'], $conf['user'], $conf['psw']);
         } catch(PDOexception $e) {
             die('Could not connect:'.$e->getMessage());
         }
